9bad40c0df5b0d35079d13e7746ea4c6f24edac2
zhangls <zhangls2014@gmail.com>
2025-11-17 18:39:49 +0800

    build: 初始化项目

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.jetbrains.kotlin)
+  alias(libs.plugins.jetbrains.compose.compiler)
+}
+
+android {
+  namespace = "me.zhangls.notes"
+  compileSdk = libs.versions.compileSdk.get().toInt()
+  buildToolsVersion = libs.versions.buildTool.get()
+
+  defaultConfig {
+    applicationId = "me.zhangls.notes"
+    minSdk = libs.versions.minSdk.get().toInt()
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  buildFeatures {
+    compose = true
+  }
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget = JvmTarget.JVM_17
+  }
+}
+
+dependencies {
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.extJunit)
+  androidTestImplementation(libs.androidx.espresso)
+
+  implementation(libs.androidx.core)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.google.material)
+
+  // Compose
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.runtime)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.preview)
+  implementation(libs.androidx.compose.constraintLayout)
+  implementation(libs.androidx.compose.material)
+  implementation(libs.androidx.compose.material.icons.core)
+  implementation(libs.androidx.compose.material.icons.extended)
+  implementation(libs.androidx.compose.activity)
+  implementation(libs.androidx.compose.lifecycle)
+}
---
+++ app/src/main/kotlin/me/zhangls/notes/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.zhangls.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+  }
+}
